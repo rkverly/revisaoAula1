db2e8bf9ac1e8125816ab508ce5cf1fed624f5e5
ninguem <ninguem>
2023-06-01 15:17:56 -0300

    Foi a dicionado o main() que receberá dois números

---
+++ main.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main(){
+    float num1, num2;
+    float soma, subtracao, multiplicacao, divisao;
+
+    printf("Digite o primeiro número: ");
+    scanf("%f", &num1);
+
+    printf("\nDigite o segundo número: ");
+    scanf("%f", &num2);
+
+
+
+    return 0;
+}
+
